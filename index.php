94a476ee70e48e807fba6c69c6b2161ec6c29812
hani <hanisyangidha@gmail.com>
2018-06-27 13:09:58 +0700

    test

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+define('BOT_TOKEN','585691080:AAEmTxrSye2KZNW4ohSewbbM4DFIaGwdKpw';
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+// read incoming info and grab the chatID
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$chatID = $update["message"]["chat"]["id"];
+//messageFromUser
+$messageFromUser = $update["message"]["text"];
+$output="";
+if ($messageFromUser=="/start") {
+	$output = urlencode("Pilih Fitur\n /check");
+}else{
+	$output="Perintah Tidak diketahui";
+}
+$reply = $output;
+// compose reply
+//cek json
+checkJSON($chatID,$update);
+// send reply
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
+function checkJSON($chatID,$update){
+	$myFile = "log.txt";
+	$updateArray = print_r($update,TRUE);
+	$fh = fopen($myFile, 'a') or die("can't open file");
+	fwrite($fh, $chatID ."\n\n");
+	fwrite($fh, $updateArray."\n\n");
+	fclose($fh);
+}
+function sendMessage(){
+	$message = "I am a baby bot.";
+	return $message;
+}
