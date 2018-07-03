80f8b5ae502c04996f5aa6f811fa4d6aa312b725
hanisyangidha <31876646+hanisyangidha@users.noreply.github.com>
2018-07-03 11:52:49 +0700

    Add files via upload

--- read.php
+++ read.php
@@ -2,7 +2,7 @@
 	require 'connection.php';
 	
 		
-			$sql = "SELECT * FROM teknisi";
+			$sql = "SELECT * FROM helpdesk,teknisi WHERE MYIR = MYIR";
 			$query = mysqli_query($conn, $sql);
 			if (!$query){
 				die  ('SQL Errror : '. mysqli_error ($conn));
@@ -10,6 +10,14 @@
 			//$response ["Data"] = array ();
 			while ($row = mysqli_fetch_array($query)) {
 				$data =array ();
+				$data["MYIR"] = $row ["MYIR"];
+				$data["No_SC"] = $row ["No_SC"];
+				$data["No_Internet"] = $row ["No_Internet"];
+				$data["Nama_Pelanggan"] = $row ["Nama_Pelanggan"];
+				$data["Alamat_Instalasi"] = $row ["Alamat_Instalasi"];
+				$data["Type_Permintaan"] = $row ["Type_Permintaan"];
+				$data["Kcontact"] = $row ["Kcontact"];
+				$data["Tanggal_Input"] = $row ["Tanggal_Input"];
 				$data["Status_permintaan"] = $row ["Status_permintaan"];
 				$data["Teknisi"] = $row ["Teknisi"];
 				$data["Keterangan_Teknisi"] = $row ["Keterangan_Teknisi"];
