7ee4ee0fee5ecbf8608b26d526589218e3dc9e44
hanisyangidha <31876646+hanisyangidha@users.noreply.github.com>
2018-07-03 13:55:58 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
-session_start();
 define('BOT_TOKEN','585691080:AAEmTxrSye2KZNW4ohSewbbM4DFIaGwdKpw');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-// read incoming info and grab the chatID
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 $chatID = $update["message"]["chat"]["id"];
-//messageFromUser
+$state=file_get_contents('https://hunhani.000webhostapp.com/getState.php?uid='.$chatID);
+$state=json_decode($state);
 $messageFromUser = $update["message"]["text"];
 $output="";
 if ($messageFromUser=="/start") {
-	$output = urlencode("Pilih Fitur\n /check");
+	$output = urlencode("Pilih Fitur\n/check\n/state");
 }
 else if ($messageFromUser=="/check") {
-	$_SESSION["check"]=1;
 	$output = urlencode("masukkan MYIR atau kode registrasi pelanggan");
+	setState($chatID,2);
+}
+else if ($messageFromUser=="/state") {
+	$output = urlencode($state->state);
 }
 else{
-	if ($_SESSION["check"]==1) {
+	if ($state->state==2) {
 		$fromdb = file_get_contents('https://hunhani.000webhostapp.com/readone.php?myir='.$messageFromUser);
 		$efromdb= json_decode($fromdb);
 		$output = urlencode(
@@ -31,23 +33,21 @@ else{
 			"tanggal input : ".$efromdb->Tanggal_Input
 
 		);
+		setState($chatID,1);
 		// ."\n".
 		// "status permintaan : ".$efromdb->Status_permintaan."\n".
 		// "nama teknisi : ".$efromdb->Teknisi."\n".
 		// "keterangan : ".$efromdb->Keterangan_Teknisi."\n".
 		// "tindak lanjut : ".$efromdb->Tindak_Lanjut
-		session_destroy();
 	}else{
 		$output="Perintah Tidak diketahui";
 	}
 }
 $reply = $output;
-// compose reply
-//cek json
-//checkJSON($chatID,$update);
-// send reply
 $sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
 file_get_contents($sendto);
+// checkJSON($chatID,$update);
+
 // function checkJSON($chatID,$update){
 // 	$myFile = "log.txt";
 // 	$updateArray = print_r($update,TRUE);
@@ -56,7 +56,6 @@ file_get_contents($sendto);
 // 	fwrite($fh, $updateArray."\n\n");
 // 	fclose($fh);
 // }
-function sendMessage(){
-	$message = "I am a baby bot.";
-	return $message;
+function setState($x,$y){
+	file_get_contents('https://hunhani.000webhostapp.com/setState.php?uid='.$x."&state=".$y);
 }
