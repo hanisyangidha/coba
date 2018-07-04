b1bf46c2f51a468ef8ff852d7c4ff6c0797786c8
hanisyangidha <31876646+hanisyangidha@users.noreply.github.com>
2018-07-04 09:20:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,14 +27,16 @@ else{
 			"alamat instalasi : ".$efromdb->Alamat_Instalasi."\n".
 			"tipe permintaan : ".$efromdb->Type_Permintaan."\n".
 			"kcontact : ".$efromdb->Kcontact."\n".
-			"tanggal input : ".$efromdb->Tanggal_Input."\n".
-			"status permintaan : ".$efromdb->Status_permintaan."\n".
-			"nama teknisi : ".$efromdb->Teknisi."\n".
-			"keterangan : ".$efromdb->Keterangan_Teknisi."\n".
-			"tindak lanjut : ".$efromdb->Tindak_Lanjut
-
+			"tanggal input : ".$efromdb->Tanggal_Input
+			
 		);
 		setState($chatID,1);
+		/*."\n".
+		"status permintaan : ".$efromdb->Status_permintaan."\n".
+			"nama teknisi : ".$efromdb->Teknisi."\n".
+			"keterangan : ".$efromdb->Keterangan_Teknisi."\n".
+			"tindak lanjut : ".$efromdb->Tindak_Lanjut*/
+
 	}else{
 		$output="Perintah Tidak diketahui";
 	}
